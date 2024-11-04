d9682435c1cb7322d98d6d6d0715301ba86b8852
Thales Monteiro Gerhardt <thales.gerhardt@el.com.br>
2024-11-04 10:21:30 -0300

    First commit

---
+++ src/main/java/com/thalesmonteiro/bookstoremaneger/BookstoremanegerApplication.java
@@ -0,0 +1,13 @@
+package com.thalesmonteiro.bookstoremaneger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoremanegerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoremanegerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thalesmonteiro/bookstoremaneger/BookstoremanegerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thalesmonteiro.bookstoremaneger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoremanegerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
